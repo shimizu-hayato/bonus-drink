397b98776e273d0e5f4be7a2e42a3badf15bfef6
shimizu <tokuteacher@gmail.com>
2018-09-21 19:21:03 +0900

    update bonus_drink.rb and spec/bonus_drink_spec.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,20 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    num = amount 
+    quot = num #商
+    count = num #合計
+    
+    while quot != 0 do
+      quot = num / 3
+      count += quot
+      rem = num % 3 # remは余り
+      num = quot + rem
+    end
+
+    return count
   end
+end
+
+if __FILE__ == $0
+    puts BonusDrink.total_count_for(100)
 end
